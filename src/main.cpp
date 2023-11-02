c1653f19e271e40e982fcbb824e52ae96ce4d2c5
Ohto-Ai <zhu.thatboy@outlook.com>
2023-11-03 01:05:48 +0800

    support rpc server

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "render.hpp"
+#include <rest_rpc.hpp>
 
 int main() {
 #ifndef RASPBERRY_PI
@@ -12,42 +13,59 @@ int main() {
 	using ohtoai::rpi::Window;
 	using ohtoai::rpi::WS2811Strip;
 	using ohtoai::rpi::Pixmap;
+	std::atomic_bool auto_render = false;
 
-	srand(time(NULL));
-
-	// Window strip(16, 32);
 	WS2811Strip strip(16, 32);
-	// WS2811StripFakeDeviceDebug strip(16, 32);
-	Window w(32, 16);
-	Window sub_w(8, 8, &w);
-	Window sub_w2(16, 8, &w);
-
 	strip.init();
-	strip.set_rotate(270);
-	strip.set_transparent(true);
+	rest_rpc::rpc_service::rpc_server server(9000, std::thread::hardware_concurrency());
 
-	w.draw("hello"_d.set_color(ohtoai::rpi::Green), 0, 0);
-	w.draw("world"_d.set_color(ohtoai::rpi::Magenta), 8, 8);
-	w.set_transparent(true);
-
-	sub_w.draw("!!"_d.set_color(ohtoai::rpi::Red), 0, 0);
-	sub_w.set_rotate(90);
-
-	auto text = 1111_d;
-	text.color = ohtoai::rpi::Yellow;
-	sub_w2.add(&text, 0, 0);
-	sub_w2.set_transparent(true);
-
-	while (true) {
-		strip.clear();
-		sub_w.move((sub_w.x() + 1) % w.width(), (sub_w.y() + 1)% w.height());
-		sub_w2.move((sub_w2.x() + 1) % w.width(), sub_w2.y());
-
-		text.set_digits(rand() % 10000);
-
-		strip.draw(w, 0, 0);
+	server.register_handler("render", [&](rest_rpc::rpc_service::rpc_conn conn){
 		strip.render();
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
+	});
+	server.register_handler("draw_pixel", [&](rest_rpc::rpc_service::rpc_conn conn, int x, int y, ohtoai::rpi::LedColor color){
+		strip.led_ref(x, y) = color;
+	});
+	server.register_handler("draw_text", [&](rest_rpc::rpc_service::rpc_conn conn, int x, int y,
+		std::string text, ohtoai::rpi::LedColor color, ohtoai::rpi::LedColor background){
+		strip.draw(ohtoai::rpi::Text4x8(text).set_color(color).set_background(background), x, y);
+	});
+	server.register_handler("draw_number", [&](rest_rpc::rpc_service::rpc_conn conn, int x, int y,
+		int number, ohtoai::rpi::LedColor color, ohtoai::rpi::LedColor background){
+		strip.draw(ohtoai::rpi::DigitGroup3x5(number).set_color(color), x, y);
+	});
+	server.register_handler("clear", [&](rest_rpc::rpc_service::rpc_conn conn){
+		strip.clear();
+	});
+	server.register_handler("width", [&](rest_rpc::rpc_service::rpc_conn conn){
+		return strip.width();
+	});
+	server.register_handler("height", [&](rest_rpc::rpc_service::rpc_conn conn){
+		return strip.height();
+	});
+	server.register_handler("pixel", [&](rest_rpc::rpc_service::rpc_conn conn, int x, int y){
+		return strip.led(x, y);
+	});
+	server.register_handler("set_rotate", [&](rest_rpc::rpc_service::rpc_conn conn, int degree, bool flip_x, bool flip_y){
+		return strip.set_rotate(degree, flip_x, flip_y);
+	});
+	server.register_handler("set_transparent", [&](rest_rpc::rpc_service::rpc_conn conn, bool transparent){
+		return strip.set_transparent(transparent);
+	});
+	server.register_handler("set_background", [&](rest_rpc::rpc_service::rpc_conn conn, ohtoai::rpi::LedColor color){
+		return strip.set_background(color);
+	});
+	server.register_handler("set_auto_render", [&](rest_rpc::rpc_service::rpc_conn conn, bool auto_render_){
+		return auto_render = auto_render_;
+	});
+
+	std::thread render_thread = std::thread([&]{
+		while (true) {
+			if (auto_render)
+				strip.render();
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
+	});
+
+	server.run();
 	return 0;
 }
